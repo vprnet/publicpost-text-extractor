a9bcf0539b093af004f619239dd3c6a40b4d1871
Jason Howes <jhowes@jasonhowes.org>
2012-12-02 12:13:19 -0500

    No need to URI encode the URL.

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'httpclient'
 require 'sinatra'
 require 'yomu'
-require 'open-uri'
 
 @@http_client = HTTPClient.new
 @@http_client.connect_timeout = 60
 
 get '/extract?*' do
-  url = URI::encode(params[:url])
+  url = params[:url]
   response = @@http_client.get(url, :follow_redirect => true)
   text = ""
+
   Timeout::timeout(60) {
     if response.content_type.include?("text/html")
       extraction_method = :text_main
