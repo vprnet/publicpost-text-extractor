f99428170e40eba8a186c99faea991396c3082b5
Matt MacDonald <matt.macdonald@gmail.com>
2012-12-28 20:29:21 -0500

    Chunking the download of the file to temp disk rather than keeping the remotely downloaded file in memory.

--- app.rb
+++ app.rb
@@ -1,26 +1,50 @@
+# encoding: UTF-8
+require 'net/https'
 require 'httpclient'
 require 'yomu'
 require 'sinatra'
 
-@@http_client = HTTPClient.new
-@@http_client.connect_timeout = 60
-
 get '/extract?*' do
-  url = URI::encode(params[:url])
-  response = @@http_client.head(url, :follow_redirect => true)
-  url = response.http_header.request_uri.to_s
-  puts ">>>> Headers for: (#{url}) #{response.headers}"
-  text = ""
+  begin
+    text = ""
 
-  if response.content_type.include?("text/html")
-    text = Yomu.new(url).text_main
-  else
-    text = Yomu.new(url).text
-  end
+    http_client = HTTPClient.new
+    http_client.connect_timeout = 60
 
-  # Sometimes we don't get enough text...
-  if text.split(" ").size < 100
-    text = Yomu.new(url).text
+    url = URI::encode(params[:url])
+    response = http_client.head(url, :follow_redirect => true)
+    guid = Digest::MD5.hexdigest(url)
+    filename = "#{settings.root}/downloaded/#{guid}-#{Time.now.to_i}"
+    file = open(filename, 'wb')
+
+    # Chunk the download out to a temp file so that we keep memory limits low
+    begin
+      http_client.get_content(url, :follow_redirect => true) do |chunk|
+        file.write(chunk)
+      end
+    ensure
+      file.close
+    end
+
+    # Extract the text from the document
+    yomu = Yomu.new filename
+    if response.content_type.include?("text/html")
+      text = yomu.text_main
+    else
+      text = yomu.text
+    end
+
+    # Sometimes we don't get enough text...
+    if text.split(" ").size < 100
+      yomu = Yomu.new filename
+      text = yomu.text
+    end
+
+  ensure
+    # Clean up the temp file
+    File.delete(filename)
+
+    #httpclient = nil
   end
 
   return safe_squeeze(text)
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require './app'
 use Rack::Deflater
+FileUtils.mkdir_p("#{settings.root}/downloaded")
 run Sinatra::Application
